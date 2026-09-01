aa18a13293eab4c3e9d0c53b8206798778c13b65
Ryan Liu <ryanliuqc@gmail.com>
2026-09-01 17:36:12 -0400

    feat: wrote SACAgent constructor, implemented select_action() method

--- src/rl/agents/sac_agent.py
+++ src/rl/agents/sac_agent.py
@@ -8,27 +8,64 @@
 import numpy as np
 from src.rl.base_agent import BaseAgent
 from src.rl.replay_buffer import ReplayBuffer
-from src.rl.networks import ActorNetwork
+from src.rl.networks import SACActor, SACCritic
+
+import torch
+from torch.nn import utils
+from torch import optim
+import torch.nn.functional as F
+
 
 class SACAgent(BaseAgent):
-    def __init__(self, obs_dim: int = 6, action_dim: int = 2, lr: float = 3e-4) -> None:
+    def __init__(
+            self, 
+            obs_dim: int = 6, 
+            action_dim: int = 2, 
+            actor_lr: float = 1e-4, 
+            critic_lr: float = 3e-4,
+            buffer_capacity: int = 100000,
+            ) -> None:
+
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() 
+            else "mps" if torch.backends.mps.is_available() 
+            else "cpu"
+        )
+
         self.obs_dim: int = obs_dim
         self.action_dim: int = action_dim
-        self.lr: float = lr
+        self.actor_lr: float = actor_lr
+        self.critic_lr: float = critic_lr
 
-        self.Q1 = ActorNetwork(obs_dim, action_dim)
-        self.Q2 = ActorNetwork(obs_dim, action_dim)
+        self.Q1 = SACCritic(obs_dim, action_dim).to(self.device)
+        self.Q2 = SACCritic(obs_dim, action_dim).to(self.device)
 
-        self.replay_buffer = ReplayBuffer(capacity=100000, obs_dim=obs_dim, action_dim=action_dim)
+        # COPY the parameters from Q1 and same thing for Q2
+        self.Q1_target = SACCritic(obs_dim=obs_dim, action_dim=action_dim).to(self.device)
+        self.Q2_target = SACCritic(obs_dim=obs_dim, action_dim=action_dim).to(self.device)
 
+        self.Q1_target.load_state_dict(self.Q1.state_dict())
+        self.Q2_target.load_state_dict(self.Q2.state_dict())
+
+        self.actor = SACActor(obs_dim=obs_dim, action_dim=action_dim)
+
+        self.replay_buffer = ReplayBuffer(capacity=buffer_capacity, obs_dim=obs_dim, action_dim=action_dim)
+
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=actor_lr)
+
+        self.Q_optimizer = torch.optim.Adam(list(self.Q1.parameters())+list(self.Q2.parameters()), lr=critic_lr)
         
-
-        # TODO: Initialize actor network, twin Q-networks, and target networks.
 
     def select_action(self, obs: np.ndarray, deterministic: bool = False) -> np.ndarray:
         """Select continuous action using reparameterized PolicyNetwork."""
-        # TODO: Forward pass through SAC actor network. Returning zeros for stub.
-        return np.zeros(self.action_dim, dtype=np.float32)
+        obs_tensor = torch.as_tensor(obs, dtype=torch.float32, device=self.device).unsqueeze(0)
+        if deterministic:
+            # maybe need to detach (no grad to make this work before converting to numpy)
+            return self.actor(obs)[0].numpy()
+
+        action, log_std = self.actor.sample_action(obs_tensor)
+
+        return action.numpy()
 
     def train_step(self, trajectory_buffer: dict) -> dict:
         """Perform SAC soft Q-learning update step over mini-batch."""
